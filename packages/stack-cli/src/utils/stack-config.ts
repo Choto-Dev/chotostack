e5b24dfafcafe043486f0c524d2cf12e28084308
Rifat Khan <54655388+StepAsideLiL@users.noreply.github.com>
2025-10-02 19:03:51 +0600

    docs(stack-cli): add comments.

--- packages/stack-cli/src/utils/stack-config.ts
+++ packages/stack-cli/src/utils/stack-config.ts
@@ -1,16 +1,29 @@
 import fs from "node:fs/promises";
 import path from "node:path";
-import { type TTemplateNamespace, templates } from "../auto-gen/templates";
+import {
+  type TTemplateNamespace,
+  type TTemplateOption,
+  templates,
+} from "../auto-gen/templates";
 
 type TChotostackConfig = {
   workspace: string;
   packages: Record<TTemplateNamespace, string>;
 };
 
+/**
+ * Define ChotoStack config Object.
+ * @param config ChotoStack config Object.
+ * @returns Config object.
+ */
 function defineChotostackConfig(config: TChotostackConfig) {
   return config;
 }
 
+/**
+ * Add package namespace in ChotoStack config.
+ * @param namespace Namespace of package.
+ */
 async function addPackageChotostackConfig(namespace: TTemplateNamespace) {
   const configFileDir = path.join(process.cwd(), "chotostack.json");
   const configFile = await fs.readFile(configFileDir, "utf-8");
@@ -22,6 +35,10 @@ async function addPackageChotostackConfig(namespace: TTemplateNamespace) {
   await fs.writeFile(configFileDir, JSON.stringify(config, null, 2), "utf-8");
 }
 
+/**
+ * Remove package namespace in ChotoStack config.
+ * @param namespace Namespace of package.
+ */
 async function removePackageFromChotostackConfig(
   namespace: TTemplateNamespace
 ) {
@@ -35,7 +52,13 @@ async function removePackageFromChotostackConfig(
   await fs.writeFile(configFileDir, JSON.stringify(config, null, 2), "utf-8");
 }
 
-async function installedPackagesInChotostackConfig() {
+/**
+ * List of templates in the project.
+ * @returns Template list with name as label and namespace as value.
+ */
+async function installedPackagesInChotostackConfig(): Promise<
+  TTemplateOption[]
+> {
   const configFileDir = path.join(process.cwd(), "chotostack.json");
   const configFile = await fs.readFile(configFileDir, "utf-8");
 
